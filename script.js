51f82e3a9f2341f10e1edd27167495b2cd4b20ee
Tyler Wiatrek <wiatrektyler@yahoo.com>
2021-11-20 11:56:14 -0600

    added js to questions

--- script.js
+++ script.js
@@ -1,51 +1,112 @@
 var startQuizButton = document.querySelector("#start-quiz");
+var questionsEl = document.getElementById("questions-main");
+var currentQuestionIndex = 0;
+var choicesEl = document.getElementById("questions-choices");
 
-function startQuiz (event) {
-    event.preventDefault();
 
 var questions = [
     {
         question: "Which data type reads true and false values?",
-        A: "String",
-        B: "Boolean",
-        C: "Object",
-        D: "Null",
+        choices: [
+            "String",
+            "Boolean",
+            "Object",
+            "Null",
+        ],
+
+        answers: "boolean"
 
     },
     {
         question: "When trying to log a value in the browser's console, what command should be used?",
-        A: "browser.log",
-        B: "log.browser",
-        C: "console.log",
-        D: "log.console",
+        choices: [
+            "brownser.log",
+            "log.browser",
+            "console.log",
+            "log.console",
+        ],
+
+        answers: "console.log"
+     
     },
     {
         question: "The user wishes to increment the value of 'i' after each iteration, what command should they use?",
-        A: "i+1",
-        B: "i++",
-        C: "increment.i",
-        D: "i+",
+        choices: [
+            "i!",
+            "i++",
+            "increment.i",
+            "i+",
+        ],
+
+        answers: "i++"
+    
     },
     {
         question: "When setting values for an array, the user should use what?",
-        A: "Curly Brackets",
-        B: "Quotations",
-        C: "Parenthesis",
-        D: "Square Brackets",
+        choices: [
+            "Curly Brackets",
+            "Quotations",
+            "Parenthesis",
+            "Square Brackets",
+        ],
+
+        answers: "Square Brackets"
+
     },
     {
         question: "The parameters of a function are enclosed in what?",
-        A: "Parenthesis",
-        B: "Square Brackets",
-        C: "Curly Brackets",
-        D: "Semi-colons",
+        choices: [
+            "Parenthesis",
+            "Square Brackets",
+            "Curly Brackets",
+            "Semi-colons",
+        ],
+
+        answers: "Parenthesis"
+       
     }
-
-
 
 ];
 
+
+function startQuiz (event) {
+    event.preventDefault();
+
+    var startScreenEl = document.getElementById("quiz-main")
+    startScreenEl.setAttribute("class", "hide");
+    questionsEl.removeAttribute("class")
+    getQuestions();
+
 }
+
+function getQuestions() {
+    var currentQuestion = questions[currentQuestionIndex];
+    var titleEl = document.getElementById("questions-title");
+    titleEl.textContent = currentQuestion.question;
+    choicesEl.innerHTML = "";
+
+    for (var i = 0;i < currentQuestion.choices.length; i++) {
+        var choicesButton = document.createElement("button");
+        choicesButton.setAttribute("class", "choicesButton");
+        choicesButton.setAttribute("value", currentQuestion.choices[i]);
+        choicesButton.textContent = i+1+": " + currentQuestion.choices[i];
+        choicesButton.onclick = questionClick();
+        choicesEl.appendChild(choicesButton);
+
+
+
+    }
+
+}
+
+function questionClick() {
+    currentQuestionIndex++;
+    getQuestions();
+
+
+}
+
+
  console.log("hello, quiz has started");
 
 //need to insert function here
